4ff5bb03ed33e8700c255c824578e8a7beedf1d9
MatiasComercioITBA <mcomerciovazque@itba.edu.ar>
2016-03-30 11:56:55 -0300

    Schema creation - just on the same schema.sql and a common DispatcherServlet.

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -1,5 +1,60 @@
-CREATE TABLE IF NOT EXISTS users (
-				userid SERIAL PRIMARY KEY,
-				username VARCHAR (100),
-				password VARCHAR (100)
+CREATE TABLE IF NOT EXISTS "user" (
+  dni INTEGER NOT NULL ,
+  first_name VARCHAR (50) NOT NULL ,
+  last_name VARCHAR (50) NOT NULL ,
+  genre CHAR (1) NOT NULL ,
+  birthdate DATE NOT NULL ,
+  email VARCHAR(100) NOT NULL,
+
+  PRIMARY KEY (dni)
+);
+
+CREATE TABLE IF NOT EXISTS address (
+  country VARCHAR(50) NOT NULL ,
+  city VARCHAR(50) NOT NULL ,
+  neighborhood VARCHAR(50) NOT NULL ,
+  street VARCHAR(100) NOT NULL,
+  number INTEGER NOT NULL,
+  floor INTEGER,
+  door INTEGER,
+  dni INTEGER NOT NULL ,
+
+  PRIMARY KEY (country, city, neighborhood, street, number, dni),
+  FOREIGN KEY (dni) REFERENCES "user" ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS student (
+  docket SERIAL NOT NULL ,
+  dni INTEGER NOT NULL ,
+  password VARCHAR (100) ,
+
+  PRIMARY KEY (docket, dni),
+  FOREIGN KEY (dni) REFERENCES "user"
+);
+
+CREATE TABLE IF NOT EXISTS course (
+  id INTEGER NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  credits INTEGER NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS inscription (
+  docket INTEGER NOT NULL,
+  course_id INTEGER NOT NULL,
+
+  PRIMARY KEY (docket, course_id),
+  FOREIGN KEY (docket) REFERENCES student ON DELETE CASCADE,
+  FOREIGN KEY (course_id) REFERENCES course ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS grade (
+  docket INTEGER NOT NULL,
+  course_id INTEGER NOT NULL ,
+  grade DECIMAL NOT NULL ,
+
+  PRIMARY KEY (docket, course_id, grade),
+  FOREIGN KEY (docket) REFERENCES student ON DELETE CASCADE,
+  FOREIGN KEY (course_id) REFERENCES course ON DELETE RESTRICT
 );
