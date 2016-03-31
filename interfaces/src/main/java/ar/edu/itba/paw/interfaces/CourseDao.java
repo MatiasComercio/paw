e525a3ff5bdbf00f2be99b0ea6e8b0ed9443f14d
mati <matimercado06@hotmail.com>
2016-03-30 21:37:18 -0300

    Added interface of CourseDAO

---
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/CourseDao.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.paw.interfaces;
+
+import ar.edu.itba.paw.models.Course;
+
+/**
+ * Created by mati on 30/03/16.
+ */
+public interface CourseDao {
+
+    Course create(int id, String coursename, int credits);
+    Course getById(int id);
+}
