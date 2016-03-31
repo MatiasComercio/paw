63bff14ebd1a57c390eca339676f06988ecd285b
gonzalo <gibarsin@itba.edu.ar>
2016-03-31 16:22:58 -0300

    added getAllCourses method

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/CourseDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/CourseDao.java
@@ -2,11 +2,16 @@ package ar.edu.itba.paw.interfaces;
 
 import ar.edu.itba.paw.models.Course;
 
+import java.util.List;
+
 /**
  * Created by mati on 30/03/16.
  */
 public interface CourseDao {
 
     Course create(int id, String coursename, int credits);
+
     Course getById(int id);
+
+    List<Course> getAllCourses();
 }
