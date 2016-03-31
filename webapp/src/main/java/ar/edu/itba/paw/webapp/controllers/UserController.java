94f0cd628ea2756f0e112198f121230a492d9e86
gonzalo <gibarsin@itba.edu.ar>
2016-03-31 15:33:55 -0300

    removed unnecesary imports

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
@@ -1,13 +1,11 @@
 package ar.edu.itba.paw.webapp.controllers;
 
-import ar.edu.itba.paw.interfaces.CourseService;
+import ar.edu.itba.paw.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import ar.edu.itba.paw.interfaces.UserService;
 
 @Controller
 public class UserController {
