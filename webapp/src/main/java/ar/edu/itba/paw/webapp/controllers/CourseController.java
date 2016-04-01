5218ed45a22006b943bda044891daa0cdf6629d3
gonzalo <gibarsin@itba.edu.ar>
2016-04-01 13:27:18 -0300

    added getCoursesByFilter modelAndView to recieve by query parameter the keyword

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/CourseController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/CourseController.java
@@ -1,10 +1,12 @@
 package ar.edu.itba.paw.webapp.controllers;
 
 import ar.edu.itba.paw.interfaces.CourseService;
+import ar.edu.itba.paw.models.CourseFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,6 +26,13 @@ public class CourseController {
     public ModelAndView getAllCourses() {
         final ModelAndView mav = new ModelAndView("courses");
         mav.addObject("courses", courseService.getAllCourses());
+        return mav;
+    }
+
+    @RequestMapping(value = "/courses", params = {"keyword"})
+    public ModelAndView getCoursesByFilter(@RequestParam String keyword) {
+        final ModelAndView mav = new ModelAndView("courses");
+        mav.addObject("courses", courseService.getByFilter(keyword));
         return mav;
     }
 }
