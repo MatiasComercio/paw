e71b45188287fe3f1c81b4723b5679440d4aba63
gonzalo <gibarsin@itba.edu.ar>
2016-04-01 17:51:40 -0300

    added id filter to CourseFilter

--- shared/src/main/java/ar/edu/itba/paw/shared/CourseFilter.java
+++ shared/src/main/java/ar/edu/itba/paw/shared/CourseFilter.java
@@ -2,20 +2,33 @@ package ar.edu.itba.paw.shared;
 
 public class CourseFilter {
     private String keyword; /* TODO: Decide whether the service is obliged to create a Filter with at least one field */
+    private int id;
 
     private CourseFilter(CourseFilterBuilder builder) {
         this.keyword = builder.keyword;
+        this.id = id;
     }
 
     public String getKeyword() {
         return keyword;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public static class CourseFilterBuilder {
         private String keyword;
+        private int id;
 
         public CourseFilterBuilder keyword(final String keyword) {
             this.keyword = keyword;
+
+            return this;
+        }
+
+        public CourseFilterBuilder id(final int id) {
+            this.id = id;
 
             return this;
         }
