1421ba776f8d926bfe58be66b2eb027d83ed7900
gonzalo <gibarsin@itba.edu.ar>
2016-04-03 16:03:13 -0300

    added forwarding from studentService implementation to studentDao

--- services/src/main/java/ar/edu/itba/paw/services/StudentServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/StudentServiceImpl.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.interfaces.StudentDao;
 import ar.edu.itba.paw.interfaces.StudentService;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.users.Student;
+import ar.edu.itba.paw.shared.StudentFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,11 @@ public class StudentServiceImpl implements StudentService {
 	@Override
 	public List<Course> getStudentCourses(int docket) {
 		return studentDao.getStudentCourses(docket);
+	}
+
+	@Override
+	public List<Student> getByFilter(StudentFilter studentFilter) {
+		return studentDao.getByFilter(studentFilter);
 	}
 
 	@Override
