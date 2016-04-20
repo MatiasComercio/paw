a077bfbd5ad6363c41c24d292eeaf60b7301069a
gonzalo <gibarsin@itba.edu.ar>
2016-04-20 18:16:18 -0300

    added ERROR_ID_OUT_OF_BOUNDS

--- shared/src/main/java/ar/edu/itba/paw/shared/Result.java
+++ shared/src/main/java/ar/edu/itba/paw/shared/Result.java
@@ -4,7 +4,8 @@ public enum Result {
     OK(""),
     COURSE_EXISTS_GRADE("Existen alumnos que poseen notas de este curso."),
     COURSE_EXISTS_INSCRIPTION("Existen alumnos que poseen están inscriptos en este curso."),
-    ERROR_UNKNOWN("Un error desconocido ha ocurrido");
+    ERROR_UNKNOWN("Un error desconocido ha ocurrido"),
+    ERROR_ID_OUT_OF_BOUNDS("El ID está fuera de los límites permitidos");
 
     private final String message;
 
