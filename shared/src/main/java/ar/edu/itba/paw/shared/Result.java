1c40e540abee47c0fbf411e1da89525e7b13571a
gonzalo <gibarsin@itba.edu.ar>
2016-04-20 19:17:31 -0300

    added error_docket

--- shared/src/main/java/ar/edu/itba/paw/shared/Result.java
+++ shared/src/main/java/ar/edu/itba/paw/shared/Result.java
@@ -5,7 +5,8 @@ public enum Result {
     COURSE_EXISTS_GRADE("Existen alumnos que poseen notas de este curso."),
     COURSE_EXISTS_INSCRIPTION("Existen alumnos que están inscriptos en este curso."),
     ERROR_UNKNOWN("Un error desconocido ha ocurrido"),
-    ERROR_ID_OUT_OF_BOUNDS("El ID está fuera de los límites permitidos");
+    ERROR_ID_OUT_OF_BOUNDS("El ID está fuera de los límites permitidos"),
+    ERROR_DOCKET_OUT_OF_BOUNDS("El legajo está fuera de los límites permitidos");
 
     private final String message;
 
