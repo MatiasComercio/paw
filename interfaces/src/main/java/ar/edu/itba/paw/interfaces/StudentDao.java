12682776a4812a79a4e7f17d320d6a0c45cb310b
gonzalo <gibarsin@itba.edu.ar>
2016-04-20 19:19:46 -0300

    added deleteStudent into DAO interface

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/StudentDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/StudentDao.java
@@ -3,6 +3,7 @@ package ar.edu.itba.paw.interfaces;
 import ar.edu.itba.paw.models.Address;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.users.Student;
+import ar.edu.itba.paw.shared.Result;
 import ar.edu.itba.paw.shared.StudentFilter;
 
 import java.time.LocalDate;
@@ -48,4 +49,11 @@ public interface StudentDao {
 	 * an empty list.
 	 */
 	List<Student> getByFilter(StudentFilter studentFilter);
+
+	/**
+	 * +++xdocument
+	 * @param docket
+	 * @return
+     */
+	Result deleteStudent(Integer docket);
 }
