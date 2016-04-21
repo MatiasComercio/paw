5f69d6658db88d99ac0d88d11180b87d99553b35
gonzalo <gibarsin@itba.edu.ar>
2016-04-21 16:29:15 -0300

    implemented GradeForm including docket, courseId and gradeForm

--- webapp/src/main/java/ar/edu/itba/paw/webapp/forms/GradeForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/forms/GradeForm.java
@@ -1,5 +1,52 @@
 package ar.edu.itba.paw.webapp.forms;
 
 
+import ar.edu.itba.paw.models.Grade;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
 public class GradeForm {
+    @NotNull
+    @Min(1)
+    private Integer docket;
+
+    @NotNull
+    @Min(1)
+    private Integer courseId;
+
+    @NotNull
+    @Min(0)
+    @Max(10)
+    private BigDecimal grade;
+
+    public Grade build() {
+        return new Grade.Builder(docket, courseId, grade).build();
+    }
+
+    public Integer getDocket() {
+        return docket;
+    }
+
+    public void setDocket(Integer docket) {
+        this.docket = docket;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public BigDecimal getGrade() {
+        return grade;
+    }
+
+    public void setGrade(BigDecimal grade) {
+        this.grade = grade;
+    }
 }
