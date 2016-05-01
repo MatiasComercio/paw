8d922a7a6ff456cc08ea92d9a0885d22fd8a4fb4
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 14:07:03 -0300

    added AdminDao interface with method GetAllAdmins

---
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/AdminDao.java
@@ -0,0 +1,17 @@
+package ar.edu.itba.paw.interfaces;
+
+
+import ar.edu.itba.paw.models.users.Admin;
+
+import java.util.List;
+
+public interface AdminDao {
+
+    /**
+     * Get all the existing administrators with their info
+     * @return  A list with the administrators;
+     *          in case there aren't any existant admins,
+     *          the list will be empty
+     */
+    List<Admin> getAllAdmins();
+}
