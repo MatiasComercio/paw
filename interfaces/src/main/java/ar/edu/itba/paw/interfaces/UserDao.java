697c82903cd2902f5f49ed88a707a2a27501277d
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 18:56:00 -0300

    added create method in UserDao

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.interfaces;
 
 import ar.edu.itba.paw.models.Role;
+import ar.edu.itba.paw.models.users.User;
+import ar.edu.itba.paw.shared.Result;
 
 import java.util.List;
 
@@ -22,4 +24,11 @@ public interface UserDao {
 	 * @return The user's roles; null if no user was found with the given dni.
 	 */
 	List<Role> getRoles(final int dni);
+
+	/**
+	 * +++xdocument
+	 * @param user
+	 * @return
+     */
+	Result create(User user);
 }
