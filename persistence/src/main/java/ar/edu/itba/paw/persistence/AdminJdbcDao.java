cccf1b6341825b81bf2823eeacf3fab1e565b2b4
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 14:13:18 -0300

    added AdminJdbcDao implementation of AdminDao

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AdminJdbcDao.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.AdminDao;
+import ar.edu.itba.paw.models.users.Admin;
+
+import java.util.List;
+
+public class AdminJdbcDao implements AdminDao {
+
+    @Override
+    public List<Admin> getAllAdmins() {
+        return null;
+    }
+}
