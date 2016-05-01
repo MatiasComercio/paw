572bd6c8dfb1b0be0635b06948ddb3704ebebc6b
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 14:49:31 -0300

    added Service annotation on adminServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/AdminServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AdminServiceImpl.java
@@ -4,9 +4,11 @@ import ar.edu.itba.paw.interfaces.AdminDao;
 import ar.edu.itba.paw.interfaces.AdminService;
 import ar.edu.itba.paw.models.users.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
