e9a8eb57e6b62a79cfc45bf8a291c2333c7defc8
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 14:09:53 -0300

    added AdminServiceImpl

---
+++ services/src/main/java/ar/edu/itba/paw/services/AdminServiceImpl.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.interfaces.AdminService;
+import ar.edu.itba.paw.models.users.Admin;
+
+import java.util.List;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public List<Admin> getAllAdmins() {
+        return null;
+    }
+}
