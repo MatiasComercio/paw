2547089b5192eb7b9b5b56210797757d5fa2c756
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 17:24:47 -0300

    added autowired AdminService

--- services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.AdminService;
 import ar.edu.itba.paw.interfaces.StudentService;
 import ar.edu.itba.paw.interfaces.UserDao;
 import ar.edu.itba.paw.interfaces.UserService;
@@ -20,6 +21,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private StudentService studentService;
+
+	@Autowired
+	private AdminService adminService;
 
 	@Override
 	public User getByDni(final String dniString) {
