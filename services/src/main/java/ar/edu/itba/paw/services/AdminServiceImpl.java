f10075dd59dfee5b2bee527017b0816b322d48f7
gonzalo <gibarsin@itba.edu.ar>
2016-05-01 14:10:48 -0300

    added adminDao autowired field

--- services/src/main/java/ar/edu/itba/paw/services/AdminServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AdminServiceImpl.java
@@ -1,11 +1,17 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.AdminDao;
 import ar.edu.itba.paw.interfaces.AdminService;
 import ar.edu.itba.paw.models.users.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    AdminDao adminDao;
+
     @Override
     public List<Admin> getAllAdmins() {
         return null;
