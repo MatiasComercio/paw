f8244f9995846aa553151a93bd7f7ea629e7ff1e
gonzalo <gibarsin@itba.edu.ar>
2016-05-02 21:39:30 -0300

    created PasswordValidator to validate newPassword and repeatNewPassword

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/forms/validators/PasswordValidator.java
@@ -0,0 +1,28 @@
+package ar.edu.itba.paw.webapp.forms.validators;
+
+import ar.edu.itba.paw.webapp.forms.PasswordForm;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class PasswordValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return PasswordForm.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        PasswordForm pw = (PasswordForm) target;
+        final String newPassword = pw.getNewPassword();
+        final String repeatNewPassword = pw.getRepeatNewPassword();
+
+        System.out.println(newPassword);
+        System.out.println(repeatNewPassword);
+
+        if(!newPassword.equals(repeatNewPassword)) {
+            errors.rejectValue("repeatNewPassword", "repeatNewPassword.notequal");
+        }
+    }
+}
