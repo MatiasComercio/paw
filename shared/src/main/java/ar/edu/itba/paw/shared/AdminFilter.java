db466d36044d1f95c5ef5d5e2394f02568743e0a
gonzalo <gibarsin@itba.edu.ar>
2016-05-05 16:23:37 -0300

    implemented Admin Filter  (extending from UserFilter)

--- shared/src/main/java/ar/edu/itba/paw/shared/AdminFilter.java
+++ shared/src/main/java/ar/edu/itba/paw/shared/AdminFilter.java
@@ -1,4 +1,27 @@
 package ar.edu.itba.paw.shared;
 
-public class AdminFilter {
+public class AdminFilter extends UserFilter {
+
+    protected AdminFilter(AdminFilterBuilder builder) {
+        super(builder);
+    }
+
+    public static class AdminFilterBuilder extends UserFilter.UserFilterBuilder<AdminFilter, AdminFilterBuilder> {
+        private Object docket;
+
+        public AdminFilterBuilder docket(final Object docket) {
+            this.docket = docket;
+
+            return this;
+        }
+
+        public AdminFilter build() {
+            return new AdminFilterBuilder(this);
+        }
+
+        @Override
+        public AdminFilterBuilder thisBuilder() {
+            return this;
+        }
+    }
 }
