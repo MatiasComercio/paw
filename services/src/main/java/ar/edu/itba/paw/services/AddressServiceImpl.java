b889e0acd1e7e63e9cb2b247c0208f93368c5332
gonzalo <gibarsin@itba.edu.ar>
2016-05-06 00:50:37 -0300

    added addressDao autowired

--- services/src/main/java/ar/edu/itba/paw/services/AddressServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AddressServiceImpl.java
@@ -1,12 +1,18 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.AddressDao;
 import ar.edu.itba.paw.interfaces.AddressService;
 import ar.edu.itba.paw.models.Address;
 import ar.edu.itba.paw.shared.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressDao addressDao;
+
     @Override
     public boolean hasAddress(Integer dni) {
         return false;
