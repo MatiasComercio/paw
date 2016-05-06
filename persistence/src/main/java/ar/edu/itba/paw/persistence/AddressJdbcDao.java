96af4189a1cefe5c5b6fd56b9c4ef1710c14c317
gonzalo <gibarsin@itba.edu.ar>
2016-05-06 00:52:34 -0300

    added AddressJdbcDao file

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AddressJdbcDao.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.AddressDao;
+import ar.edu.itba.paw.models.Address;
+import ar.edu.itba.paw.shared.Result;
+
+public class AddressJdbcDao implements AddressDao {
+
+    @Override
+    public boolean hasAddress(Integer dni) {
+        return false;
+    }
+
+    @Override
+    public Result createAddress(Integer dni, Address address) {
+        return null;
+    }
+
+    @Override
+    public Result updateAddress(Integer dni, Address address) {
+        return null;
+    }
+}
