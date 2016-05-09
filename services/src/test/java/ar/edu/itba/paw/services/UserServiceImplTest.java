7e216d28d13c282509e29d03b087d7a182160f66
gonzalo <gibarsin@itba.edu.ar>
2016-05-08 23:07:46 -0300

    added testGetRoles method with userDao @Autowired

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -1,5 +1,16 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.UserDao;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserServiceImplTest {
 
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testGetRoles() {
+
+    }
 }
