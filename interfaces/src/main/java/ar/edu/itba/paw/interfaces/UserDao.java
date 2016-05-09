955398cbb0b362a41db4dd2c2589fb1ff1062e88
gonzalo <gibarsin@itba.edu.ar>
2016-05-08 22:54:31 -0300

    removed tag

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
@@ -25,7 +25,6 @@ public interface UserDao {
 	 */
 	<V extends User, T extends User.Builder<V,T>> V getByDni(int dni, User.Builder<V, T> builder);
 
-	/* +++xtest *//* +++xchange: should return only one role */
 	/**
 	 * Gets all the roles the user with the given dni has associated.
 	 * @param dni The user's dni
