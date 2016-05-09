bff0bfbd4d2bcab817b5c6a23e63f057cd065ebe
gonzalo <gibarsin@itba.edu.ar>
2016-05-08 23:10:10 -0300

    changed annotation from @Autowired to @Mock

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -2,12 +2,14 @@ package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.UserDao;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImplTest {
 
-    @Autowired
+    @Mock
     private UserDao userDao;
+
 
     @Test
     public void testGetRoles() {
