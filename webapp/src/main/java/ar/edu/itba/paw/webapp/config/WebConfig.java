412b73188459d8fd7727615742735ed42720565b
MatiasComercioITBA <mcomerciovazque@itba.edu.ar>
2016-05-26 13:05:54 -0300

    Modified - WebConfig is set for working with Hibernate too.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.webapp.config;
 
 import org.postgresql.Driver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,10 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
@@ -23,8 +29,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 @ComponentScan({
 		"ar.edu.itba.paw.webapp.controllers",
@@ -36,6 +44,7 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 @EnableTransactionManagement
 public class WebConfig extends WebMvcConfigurerAdapter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
 
 	// equivalents for <mvc:resources/> tags
 	@Override
@@ -49,9 +58,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		configurer.enable();
 	}
 
-	@Value("classpath:schema.sql")
-	private Resource schemaSql;
-
+	/* Set path to .jsp files */
 	@Bean
 	public ViewResolver viewResolver() {
 		final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -60,34 +67,83 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	/* /Set path to .jsp files */
 
+	/* Database Connection */
 	@Bean
 	public DataSource dataSource() {
 		final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/paw");
-		dataSource.setUsername("paw");
-		dataSource.setPassword("paw01");
-/*		dataSource.setUrl("jdbc:postgresql://10.16.1.110:5432/grupo1");
-		dataSource.setUsername("grupo1");
-		dataSource.setPassword("OoLuej2w");*/
+
+		if (isDevelopmentMode()) {
+			dataSource.setUrl("jdbc:postgresql://localhost:5432/paw");
+			dataSource.setUsername("paw");
+			dataSource.setPassword("paw01");
+		} else {
+			dataSource.setUrl("jdbc:postgresql://10.16.1.110:5432/grupo1");
+			dataSource.setUsername("grupo1");
+			dataSource.setPassword("OoLuej2w");
+		}
 
 		return dataSource;
 	}
+	/* /Database Connection */
 
+	/* JDBC Beans */ // +++xremove
+//	@Value("classpath:schema.sql")
+//	private Resource schemaSql;
 	@Bean
 	public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
 		final DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
 		dataSourceInitializer.setDataSource(dataSource);
-		dataSourceInitializer.setDatabasePopulator(databasePopulator());
+//		dataSourceInitializer.setDatabasePopulator(databasePopulator());
 		return dataSourceInitializer;
 	}
 
-	private DatabasePopulator databasePopulator() {
-		final ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-		databasePopulator.addScript(schemaSql);
-		return databasePopulator;
+//	private DatabasePopulator databasePopulator() {
+//		final ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+//		databasePopulator.addScript(schemaSql);
+//		return databasePopulator;
+//	}
+	/* /JDBC Beans */
+
+	/* JPA Beans */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+
+		emf.setPackagesToScan("ar.edu.itba.paw.models");
+		emf.setDataSource(dataSource());
+
+		final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emf.setJpaVendorAdapter(vendorAdapter);
+
+		// tell Hibernate how he should behave
+		final Properties hibernateProperties = new Properties();
+
+		// adds new columns and entities,
+		// but it neither drops/deletes unused tables or columns nor add indexes
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+		// set hibernate dialect; tells which database implementation we are using
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL92Dialect");
+
+		if (isDevelopmentMode()) {
+			// never show this on production
+			hibernateProperties.setProperty("hibernate.show.sql", "true");
+			hibernateProperties.setProperty("format_sql", "true");
+		}
+
+		emf.setJpaProperties(hibernateProperties);
+
+		return emf;
 	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+	/* /JPA Beans */
 
 	@Bean
 	public MessageSource messageSource() {
@@ -98,8 +154,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		return messageSource;
 	}
 
-	@Bean
-	public PlatformTransactionManager transactionManager(final DataSource ds) {
-		return new DataSourceTransactionManager(ds);
+	private static boolean isDevelopmentMode() {
+		// +++xcheck both on development & production mode
+		// use LOGGER debug mode for switching this on/off development/production mode respectively
+		return LOGGER.isDebugEnabled();
 	}
 }
